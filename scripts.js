c2c189af06f2035b756c59ad0932979165eb73b6
Robert Cordray <robertcordray@gmail.com>
2017-07-28 13:35:34 -0500

    jquery selectors

---
+++ scripts.js
@@ -0,0 +1,12 @@
+console.log('javascript has been sourced');
+$(document).ready(function() {
+
+
+    $('body').append('<h2>Hey there!</h2>');
+    $('body').prepend('<h2>I am going to the top</h2>');
+    $('li').css('color', 'blue');
+    $('#last3 li').css('color', 'maroon');
+    $('.food').css('color', 'pink');
+    $('#chief').css('color', 'green');
+    $('ul').first().children().last().css('color', 'black');
+});
